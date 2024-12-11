289063335ca6310ad258d57df26405b34152334e
RamonSanGabriel <jeshero11@gmail.com>
2024-12-11 15:34:20 +0800

    statusfontcolor

--- src/components/SelectStatus/SelectStatus.jsx
+++ src/components/SelectStatus/SelectStatus.jsx
@@ -11,21 +11,51 @@ const SelectStatus = () => {
     dispatch(localAction(e.target.value));
   };
 
-  const statusFontColor = () => {
-    if (status === 'success') {
-      return 'green';
-    }
+  const statusFontColor = (className) => {
+    const colors = {
+      [css.success]: 'green',
+      [css.failed]: 'orange',
+      [css.error]: 'red',
+    };
+
+    return colors[className] ?? 'white';
   };
+
   return (
     <div>
-      <select value={status} onChange={handleChange}>
-        <option value="Success">
+      <select
+        value={status}
+        onChange={handleChange}
+        style={{
+          color: statusFontColor(status),
+          backgroundColor: 'transparent',
+          border: 'none',
+          padding: '0.5rem',
+          borderRadius: '0.5rem',
+        }}
+        className={css.selectStatus}
+      >
+        <option className={css.success} value="Success">
           <p>Success</p>
         </option>
-        <option value="Failed">Failed</option>
-        <option value="Error">Error</option>
+        <option className={css.failed} value="Failed">
+          Failed
+        </option>
+        <option className={css.error} value="Error">
+          Error
+        </option>
       </select>
-      <p>Current status: {status}</p>
+      <p
+        className={css.currentStatus}
+        style={{
+          color: statusFontColor(css.currentStatus),
+          fontWeight: 'bold',
+          fontSize: '1.2rem',
+          marginBottom: '1rem',
+        }}
+      >
+        Current status: {status}
+      </p>
       <hr />
       <hr />
       <hr />
